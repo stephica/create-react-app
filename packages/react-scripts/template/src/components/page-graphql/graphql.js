1e6b16968707f828660bc800c03f2f6e773468ca
John D. Storey <johndangerstorey@gmail.com>
2017-05-02 15:15:03 -0600

    removes semi

--- packages/react-scripts/template/src/components/page-graphql/graphql.js
+++ packages/react-scripts/template/src/components/page-graphql/graphql.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { string, func } from 'prop-types';
-import Buffer from '../balanc3-components/buffer';
+import React from 'react'
+import { string, func } from 'prop-types'
+import Buffer from '../balanc3-components/buffer'
 
 const GraphQl = ({ address, setAddress }) => (
   <Buffer>
     <h2>GraphQl</h2>
     <input
       value={address}
-      onChange={e => setAddress(e.target.value)}
+      onChange={(e) => setAddress(e.target.value)}
       style={{ width: '400px' }}
     />
   </Buffer>
-);
+)
 
 GraphQl.propTypes = {
   address: string,
   setAddress: func
-};
+}
 
-export default GraphQl;
+export default GraphQl
