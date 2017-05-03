05b97a0af41ae23eec20586f89d8126a86308213
John D. Storey <johndangerstorey@gmail.com>
2017-05-03 12:23:44 -0600

    capitalizes App

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import App from './App'
 import { Provider } from 'react-redux'
 import configureStore from './store/configure-store'
 import { ThemeProvider } from 'styled-components'
