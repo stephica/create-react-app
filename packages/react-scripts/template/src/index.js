a42cb27d916a21e316e8418907aa11d0ca49af67
John D. Storey <johndangerstorey@gmail.com>
2017-05-04 15:21:48 -0600

    allows eslint to be used in template

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { Provider } from 'react-redux'
 import configureStore from './store/configure-store'
 import { ThemeProvider } from 'styled-components'
 import theme from './components/base/theme'
