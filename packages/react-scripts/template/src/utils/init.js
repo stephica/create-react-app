9e23d7d6902d7934ae316d78edd6ee57a1d3e190
John D. Storey <johndangerstorey@gmail.com>
2017-05-11 19:15:42 -0600

    cleans up code a bit

--- packages/react-scripts/template/src/utils/init.js
+++ packages/react-scripts/template/src/utils/init.js
@@ -1,62 +1,26 @@
 import { dispatch } from '../utils'
 import baseStyles from '../components/base/base-styles'
-import { getUserToken } from '../components/balanc3-components/account/reducers'
-import { userReceived, showLoginModal } from '../components/balanc3-components/account/reducers'
-import gql from 'graphql-tag'
+import { getUserToken, userReceived, showLoginModal } from '../components/balanc3-components/account/reducers'
 import { graphqlUrl } from '../components/base/config'
 
 export default () => {
-  console.log('called once!')
-  // const props = this.props
-  // const user = props.data.userAuths
-  // const error = props.data.error
-  // if (user) dispatch(userReceived(user))
-  // if (error) console.log('Handle Error in App.js => Maybe just stop component from rendering...')
-  //  "query": "query ($sender: String, $notRecipients: [String]) { getBySenderExcludeRecipients(sender: $sender, notRecipients: $notRecipients) {sender, recipient, tokenStandard, parentTrace} }",
-  //   "variables": {
-  //       "sender": "0xc7d3e431be6222543364408a94c12c0d089be305",
-  //       "notRecipients": ["0xc8ebccc5f5689fa8659d83713341e5ad19349448"]
-  //   }
-
-  const query = {
-    query: 'query ($token: String) { userAuths(token: $token) {_id, name, email, createdDate} }',
-    variables: {
-      token: getUserToken() || ''
-    }
-  }
-
-  //   {
-  //     "query": "query ($token: String) { userAuths(token: $token) {_id, name, email, createdDate} }",
-  //     "variables": {
-  //         "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU5MTM0NjUyYjM1MGJlMWEwNDM3OGFiNiIsImlhdCI6MTQ5NDQzNTY0NywiZXhwIjoxNDk1MDQwNDQ3fQ.WjNEbbrOqLytYvGKyZ6oLVhcVTVty49HCDaA5QfGWFE"
-  //     }
-  // }
-
-  // const query = {
-  //   'query': 'query ($sender: String) { getBySender(sender: $sender) {sender, recipient, tokenStandard, parentTrace} }',
-  //   'variables': {
-  //     'sender': '0xc7d3e431be6222543364408a94c12c0d089be305'
-  //   }
-  // }
-  console.log('query:', query)
-  // const url = `${graphqlUrl}?query={userAuthstoken:"${getUserToken()}"){_id,name,email,createdDate}}}`
-  // fetch(url)
-
+  // CHECKS IF USER IS AUTHENTICATED
   fetch(graphqlUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(query)
+    body: JSON.stringify({
+      query: 'query ($token: String) { userAuths(token: $token) {_id, name, email, createdDate} }',
+      variables: {
+        token: getUserToken() || ''
+      }
+    })
   })
     .then(res => res.json())
-    .then(res => console.log('res:', res))
-  // const AppWithUser = graphql(getUserQuery, {
-  //   options: {
-  //     variables: {
-  //       token: getUserToken()
-  //     }
-  //   }
-  // })(App)
+    .then(res => {
+      dispatch(userReceived(res.data.userAuths))
+    })
 
+  // CHECKS IF WE SHOULD DISPATCH SPECIAL NON-ROUTE ACTION
   const path = window.location.pathname
   if (path.includes('reset')) {
     dispatch(showLoginModal('reset'))
@@ -65,5 +29,7 @@ export default () => {
   } else if (path.includes('login')) {
     dispatch(showLoginModal('login'))
   }
+
+  // DOES BASE STYLES FOR SEMANTIC UI
   baseStyles()
 }
